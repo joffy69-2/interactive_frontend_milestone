a6c292d88185b65df95c961aeddd8e40f3a3fc29
joffy69 <jcwgibbs@gmail.com>
2021-08-15 17:30:11 +0000

    removed all testing code

--- assets/java.js
+++ assets/java.js
@@ -2,12 +2,14 @@ function generate_array() {
 
     //generates an empty array of length set by user input on index page
     let startlev = document.getElementById('g-level').value;
-    var game_array = new Array(); //might need var rather than let (block v global scope)
+    let game_array = new Array(); //might need var rather than let (block v global scope)
     game_array.length = startlev;
     console.log(game_array);
     //https://www.ma-no.org/en/programming/javascript/how-to-populate-an-array-with-random-numbers-in-javascript
     let myarray = Array.from({
         length: startlev
+        //https://www.educba.com/random-number-generator-in-javascript/
+
     }, () => Math.floor(Math.random() * (4)) + 1);
     myarray.forEach(function (item, index, arr) {
         console.log(item);
@@ -16,30 +18,10 @@ function generate_array() {
 
 }
 
-function generate_random() {
-    //this needs to generate a random color from 4 colors
-    let blue = 1;
-    let green = 2;
-    let red = 3;
-    let yellow = 4;
-    //https://www.educba.com/random-number-generator-in-javascript/
-    let test = Math.floor(Math.random() * (4)) + 1; //hopefully this produces a number between 1 and 4
-    alert(test);
-}
-
-function test() {
-    console.log('test');
-}
-
 document.getElementById("start").onclick = function () {
     generate_array()
 };
-//for (let i=0; i<10; i++){
-//game_array.push(generate_random())
-//}
 
 
 
-//going to be something like game_array.append(generate_random)
-//startlev needs to be in generate_random, so you generate
-//i need to push onto the array game_array.push()
+
